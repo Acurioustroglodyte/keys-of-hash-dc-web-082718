b4b39510a434305f50fa776f66fede3e17dd1587
Trevor Jameson <tjameson15@gmail.com>
2018-08-03 18:11:32 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*hash, arguments)
+    binding.pry
   end
 end
+
+# Getting undefined method error for below method call
+# Hash.keys_of({"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"})
